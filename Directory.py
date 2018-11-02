be96ec0a71e8bc73eefde991192620e8daf6fd9a
Benjamin Amos <benjamin.oxi@gmail.com>
2018-11-01 23:38:11 -0400

    new behav search by string

---
+++ Directory.py
@@ -0,0 +1,139 @@
+import time
+import re
+from spade.agent import Agent
+from spade.behaviour import OneShotBehaviour
+from spade.behaviour import CyclicBehaviour
+from spade.message import Message
+from spade.template import Template
+
+import json
+
+TIMEOUT = 100000000
+
+_filesExtension = {
+    'image': ['png', 'jpg', 'jpeg', 'tiff', 'tif', 'gif', 'svg'],
+    'text': ['txt', 'docx', 'doc', '1st', 'latex', 'md5.txt', 'odt', 'tex'],
+    'sound': ['mp3', 'flac', 'm4a', 'm4p', 'm4r', 'mid', 'midi', 'vlc', 'wav', 'wave', 'wma'],
+    'video': ['mpg', 'mov', 'wmv', 'rm', '3gp', 'avi', 'hdv', 'mp4', 'mpeg4'],
+    'pdf': ['pdf']
+}
+
+
+def cleanSender(sender):
+    return str(sender).split("/")[0]
+
+
+class DirectoryAgent(Agent):
+    direc = dict()
+
+    class NewFileBehav(CyclicBehaviour):
+        def getDirec(self, Dir):
+            self.direc = Dir
+
+        async def run(self):
+            msg = await self.receive(timeout=TIMEOUT)
+            if msg:
+                title = msg.get_metadata("title")
+                body = msg.body
+                sender = cleanSender(msg.sender)
+                print("## REQUEST FOR NEW FILE")
+                print("title of new file: {}".format(title))
+                print("from: {}".format(sender))
+                print()
+
+                if(title in self.direc):
+                    self.direc[title].append(sender)
+                    print("File already exists!")
+                    print("Peer added to the list of peers")
+                else:
+                    self.direc[title] = [sender]
+                    print("File added to directory!")
+
+    class AskFileBehav(CyclicBehaviour):
+        def getDirec(self, Dir):
+            self.direc = Dir
+
+        async def run(self):
+            msg = await self.receive(timeout=TIMEOUT)
+            if msg:
+                title = msg.get_metadata("title")
+                sender = cleanSender(msg.sender)
+
+                print("## REQUEST FOR GETTING A FILE")
+                print("title of asked file: {}".format(title))
+                print("from: {}".format(sender))
+
+                if title in self.direc:
+                    listOfPeers = self.direc[title]
+                else:
+                    listOfPeers = []
+                stringList = json.dumps(listOfPeers)
+
+                msg = Message(to=sender)
+                msg.set_metadata("job", "listOfPeers")
+                msg.set_metadata("title", title)
+                msg.set_metadata("list", stringList)
+                await self.send(msg)
+            print()
+
+    class SearchFilesBehav(CyclicBehaviour):
+        def getDirec(self, Dir):
+            self.direc = Dir
+
+        def findFilesByNamePart(self, fileNamePart):
+            existingFiles = self.direc.keys()
+            return [file for file in existingFiles if fileNamePart.lower() in file.lower()]
+
+        async def run(self):
+            msg = await self.receive(timeout=TIMEOUT)
+            if msg:
+                search = msg.get_metadata("search")
+                sender = cleanSender(msg.sender)
+
+                print("## REQUEST FOR SEARCHING FOR FILES")
+                print(f"Searching files by {search}")
+                print(f"from {sender}")
+
+                listOfFiles = self.findFilesByNamePart(search)
+                stringList = json.dumps(listOfFiles)
+
+                msg = Message(to=sender)
+                msg.set_metadata("job", "listOfFiles")
+                msg.set_metadata("search", search)
+                msg.set_metadata("files", stringList)
+                print(f"Sending {msg}")
+                await self.send(msg)
+            print()
+
+    def setup(self):
+        print("Directory started")
+        newFBehav = self.NewFileBehav()
+        newFBehav.getDirec(self.direc)
+        newFTemp = Template()
+        newFTemp.set_metadata("job", "newFile")
+        self.add_behaviour(newFBehav, newFTemp)
+
+        askFBehav = self.AskFileBehav()
+        askFBehav.getDirec(self.direc)
+        askFTemp = Template()
+        askFTemp.set_metadata("job", "askFile")
+        self.add_behaviour(askFBehav, askFTemp)
+
+        searchFBehav = self.SearchFilesBehav()
+        searchFBehav.getDirec(self.direc)
+        searchFTemp = Template()
+        searchFTemp.set_metadata("job", "searchFiles")
+        self.add_behaviour(searchFBehav, searchFTemp)
+
+
+if __name__ == "__main__":
+    directory = DirectoryAgent("13-10665@jabber.at", "operativos3")
+    directory.start()
+
+    while True:
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            directory.stop()
+            break
+    print("Agents finished")
---
+++ Peer.py
@@ -0,0 +1,113 @@
+import time
+from spade.agent import Agent
+from spade.behaviour import OneShotBehaviour
+from spade.behaviour import CyclicBehaviour
+from spade.message import Message
+from spade.template import Template
+
+import json
+
+TIMEOUT = 100000000
+
+
+class PeerAgent(Agent):
+    class PeerBehav(CyclicBehaviour):
+        async def run(self):
+            print("Options:")
+            print("1: Add new file")
+            print("2: Ask for file")
+            print("3: Search for files")
+            option = input("select option: ")
+            print()
+            if option == "1":
+                await self.addFileToDirectory()
+            elif option == "2":
+                await self.askFileFromDirectory()
+            elif option == "3":
+                await self.searchFilesInDirectory()
+            else:
+                print("############### Invalid Option ###############")
+                print()
+
+        async def addFileToDirectory(self):
+            print("############### Add new file ###############")
+            msg = Message(to="13-10665@jabber.at")
+            msg.set_metadata("job", "newFile")
+            title = input("title: ")
+            msg.set_metadata("title", title)
+            await self.send(msg)
+            print()
+
+        async def askFileFromDirectory(self):
+            print("############### Ask file ###############")
+            msg = Message(to="13-10665@jabber.at")
+            msg.set_metadata("job", "askFile")
+            title = input("title: ")
+            msg.set_metadata("title", title)
+            await self.send(msg)
+
+            msg = await self.receive(timeout=TIMEOUT)
+            if msg:
+                print()
+                print("Received list of peers")
+
+                listOfPeers = json.loads(msg.get_metadata("list"))
+
+                print("list:")
+                for peer in listOfPeers:
+                    print("+ {}".format(peer))
+
+                print("TODO: Get file from peer and inform directory")
+            print()
+
+        async def searchFilesInDirectory(self):
+            print("############### Search file(s) by name ###############")
+            msg = Message(to="13-10665@jabber.at")
+            msg.set_metadata("job", "searchFiles")
+            search = input("search by: ")
+            msg.set_metadata("search", search)
+            await self.send(msg)
+
+            msg = await self.receive(timeout=TIMEOUT)
+            if msg:
+                print()
+                print("Received list of files")
+
+                listOfFiles = json.loads(msg.get_metadata("files"))
+
+                print("Files matching your search:")
+                for file in listOfFiles:
+                    print(f"+ {file}")
+
+                print()
+                self.askFileFromDirectory()
+
+            print()
+
+    def setup(self):
+        print("PeerAgent started")
+        peerBehav = self.PeerBehav()
+
+        listOfPeersTemp = Template()
+        listOfPeersTemp.set_metadata("job", "listOfPeers")
+        # self.add_behaviour(peerBehav, listOfPeersTemp)
+
+        listOfFilesTemp = Template()
+        listOfFilesTemp.set_metadata("job", "listOfFiles")
+        self.add_behaviour(peerBehav, listOfPeersTemp | listOfFilesTemp)
+
+
+if __name__ == "__main__":
+    n = int(input("agent: "))
+    jid, pwd = "13-10665+"+str(n)+"@jabber.at", "operativos3"
+
+    peerAgent = PeerAgent(jid, pwd)
+    peerAgent.start()
+
+    while True:
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            peerAgent.stop()
+            break
+    print("Agents finished")
